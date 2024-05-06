2771c479044a2e7ae0974c67c6afa2086810afb5
walid daou <walid.daou2023@gmail.example>
2024-05-06 14:48:26 +0300

    add alpine and fancyapp links

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,16 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/carousel/carousel.css" />
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <!-- fonts -->
    
     @vite('resources/scss/app.scss', 'resources/js/app.js')
-    
+
     @yield("head")
+    
 
 </head>
 
 <body>
 
-@include('partial.header')
+    @include('partial.header')
 
     @yield("content")
 
